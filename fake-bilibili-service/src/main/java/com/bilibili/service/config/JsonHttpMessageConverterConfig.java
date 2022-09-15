46ff4b27ee31e3f68747c2c17a7a83b3b16e3a2b
wuzihong <ad800911@126.com>
2022-09-15 23:14:24 +0800

    通用功能与配置

---
+++ fake-bilibili-service/src/main/java/com/bilibili/service/config/JsonHttpMessageConverterConfig.java
@@ -0,0 +1,56 @@
+package com.bilibili.service.config;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class JsonHttpMessageConverterConfig {
+
+    // DisableCircularReferenceDetect 作用展示demo
+    public static void main(String[] args){
+        List<Object> list = new ArrayList<>();
+        Object o = new Object();
+        list.add(o);
+        list.add(o);
+        System.out.println(list.size());
+        //[{},{"$ref":"$[0]"}]    不关闭循环引用检测
+        //[{},{}]                 关闭循环引用检测
+        // fastJson在发现前面已经有相同的序列化元素时，后一个就会返回它的引用
+        System.out.println(JSONObject.toJSONString(list));
+        System.out.println(JSONObject.toJSONString(list, SerializerFeature.DisableCircularReferenceDetect));
+    }
+
+    @Bean
+    @Primary
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        // WriteNullStringAsEmpty  将null转成空字符串，这样就能让前端看到了
+        // MapSortField 对map根据key做一个升序的排序
+        // DisableCircularReferenceDetect 关闭循环引用检测
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.MapSortField,
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        //如果使用feign进行微服务间的接口调用，则需要加上该配置
+        fastConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return new HttpMessageConverters(fastConverter);
+    }
+}
---
+++ fake-bilibili-service/src/main/java/com/bilibili/service/handler/CommonGlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.bilibili.service.handler;
+
+import com.bilibili.domain.JsonResponse;
+import com.bilibili.exception.ConditionException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CommonGlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public JsonResponse<String> commonExceptionHandler(HttpServletRequest request, Exception e){
+        String errorMsg = e.getMessage();
+        if (e instanceof ConditionException) {
+            String errorCode = ((ConditionException)e).getCode();
+            return  new JsonResponse<>(errorCode, errorMsg);
+        }
+        return new JsonResponse<>("500",errorMsg);
+    }
+
+}
